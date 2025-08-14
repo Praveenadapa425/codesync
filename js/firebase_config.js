b881059ab2fe550c03c9270b19c6a680e4b928ce
Praveen Adapa <147489706+Praveenadapa425@users.noreply.github.com>
2025-08-14 21:41:05 +0530

    Create firebase_config.js using the firebase

---
+++ js/firebase_config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA7U8pX63GNDRQ-1QYo--bvJx73eIyXcjU",
+  authDomain: "codesync-88579.firebaseapp.com",
+  projectId: "codesync-88579",
+  storageBucket: "codesync-88579.firebasestorage.app",
+  messagingSenderId: "1078739088553",
+  appId: "1:1078739088553:web:0aa9b33e99a432ad7325b8"
+};
+
+//Initialise Firebase
+const app = initializeApp(firebaseConfig);
